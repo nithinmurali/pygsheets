619697d6cddbd1b41df8a1998a344318512e1c0e
nithinmurali <imnmfotmal@gmail.com>
2016-11-03 21:19:33 +0530

    adding git offline data

--- test/manual_test.py
+++ test/manual_test.py
@@ -13,15 +13,20 @@ import pygsheets
 gc = pygsheets.authorize(sfile='./data/creds.json', application_name='testapp1')
 
 # wks = gc.open_by_key('18WX-VFi_yaZ6LkXWLH856sgAsH5CQHgzxjA5T2PGxIY')
-wks =gc.open('pygsheetTest')
-print wks
+ss =gc.open('pygsheetTest')
+print ss
+try:
+    ss.del_worksheet(ss.worksheet_by_title('testtt'))
+except:
+    pass
+ss.add_worksheet('testtt',50,50)
 
-s1 = wks.sheet1
-print s1
+# wks = ss.sheet1
+# print wks
 
 # s1.update_acell('A1',"yoyo")
 # print s1.col_values(2,"cell")
 # s1.update_cell(5,5,"again yoypo")
 # s1.insert_cols(3,1)
-
+pass
 IPython.embed()
