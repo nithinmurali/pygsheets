785f7b380aaa04a0236a546cb9a9978603cea942
nithinmurali <imnmfotmal@gmail.com>
2016-11-03 21:18:58 +0530

    improved offline tests still not working

--- test/offline_tests.py
+++ test/offline_tests.py
@@ -27,6 +27,24 @@ def read_config(filename):
     config.readfp(open(filename))
     return config
 
+config = None
+gc = None
+
+
+def setup_module(module):
+    global config, gc
+    try:
+        config = read_config(CONFIG_FILENAME)
+        gc = pygsheets.authorize(CREDS_FILENAME)
+    except IOError as e:
+        msg = "Can't find %s for reading test configuration. "
+        raise Exception(msg % e.filename)
+
+    config_title = config.get('Spreadsheet', 'title')
+    sheets = [x for x in gc._spreadsheeets if x["name"] == config_title]
+    for sheet in sheets:
+        gc.delete(sheet['name'])
+
 
 class TestSpreadsheet(object):
 
@@ -39,18 +57,29 @@ class TestSpreadsheet(object):
         spreadsheet_json["Sheets"].append(worksheet_json)
 
         cls.config = read_config(CONFIG_FILENAME)
-        cls.gc = mock.create_autospec(pygsheets.Client)
+        # cls.gc = mock.create_autospec(pygsheets.Client)
+        # cls.gc.open_by_key.return_value = spreadsheet_json
+        # cls.gc._fetch_sheets = [{'testssheetid': 'testssheettitle'}]
+        # cls.spreadsheet = pygsheets.Spreadsheet(cls.gc)
+
+        cls.gc.service.spreadsheets().get = mock.create_autospec(cls.gc.service.spreadsheets().get)
         cls.gc.open_by_key.return_value = spreadsheet_json
         cls.gc._fetch_sheets = [{'testssheetid': 'testssheettitle'}]
         cls.spreadsheet = pygsheets.Spreadsheet(cls.gc)
 
-    def test_open(self):
+    @mock.patch('service.spreadsheets().get')
+    def test_open(self, mock_get):
+
+        mock_response = mock.Mock()
+        mock_response.execute.return_value = self.spreadsheet
+        mock_get.return_value = mock_response
+
         spreadsheet = self.gc.open('testssheettitle')
+
+        mock_get.assert_called_once_with(spreadsheetId=self.config.get('Spreadsheet', 'key'),
+                                         fields='properties,sheets/properties,spreadsheetId')
         assert(isinstance(spreadsheet, pygsheets.Spreadsheet))
 
     @classmethod
     def teardown_class(cls):
         pass
-
-if __name__ == '__main__':
-    unittest.main()
