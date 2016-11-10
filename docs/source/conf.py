ff36661964539bd1bbfab8d141fbc60d7e36119a
nithinmurali <imnmfotmal@gmail.com>
2016-11-11 05:08:59 +0530

    doc theme fix1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -108,7 +108,7 @@ pygments_style = 'sphinx'
 #keep_warnings = False
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
-todo_include_todos = False
+# todo_include_todos = False
 
 
 # -- Options for HTML output ----------------------------------------------
@@ -116,7 +116,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 # html_theme = 'alabaster'
-html_theme = 'default'
+# html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
