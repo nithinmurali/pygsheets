2817e7295f60a8545cf098aed11ce09cc19a8da7
Kordishal <jonaswaeber@gmail.com>
2018-03-14 12:18:22 +0100

    Updated docs get values.

--- pygsheets/worksheet.py
+++ pygsheets/worksheet.py
@@ -256,28 +256,29 @@ class Worksheet(object):
 
     def get_values(self, start, end, returnas='matrix', majdim='ROWS', include_empty=True, include_all=False,
                    value_render=ValueRenderOption.FORMATTED):
-        """Returns value of cells given the topleft corner position
-        and bottom right position
+        """Returns a defined range of values.
 
-        :param start: topleft position as tuple or label
-        :param end: bottomright position as tuple or label
-        :param majdim: output as rowwise or columwise, only for matrix
-                       takes - 'ROWS' or 'COLMUNS'
-        :param returnas: return as list of strings of cell objects
-                         takes - 'matrix', 'cell', 'range'
-        :param include_empty: include empty trailing cells/values until last non-zero value, wont fill completely empty
-                              rows ignored if inclue_all is True, this wont fill empty rows
-        :param include_all: include all the cells (even rows with no values) in the range empty/non-empty,
-                            will return exact rectangle
-        :param value_render: format of output values
-
-        Example:
+        Returns a range of values from start Cell to end Cell. It will fetch these values from remote and then
+        processes them. Will return either a simple list of lists, a list of Cell objects or a DataRange object with
+        all the cells inside.
 
         >>> wks.get_values((1,1),(3,3))
         [[u'another look.', u'', u'est'],
          [u'EE 4212', u"it's down there "],
          [u'ee 4210', u'somewhere, let me take ']]
 
+        :param start:           Top left cell as coordinates or label.
+        :param end:             Bottom right cell as coordinates or label.
+        :param majdim:          The major dimension of the matrix. ('ROWS' or 'COLUMNS')
+        :param returnas:        The type to return the fetched values as. ('matrix', 'cell', 'range')
+        :param include_empty:   Include empty cells up to 'self.cols'.
+        :param include_all:     Include empty rows to return an exact rectangle.
+        :param value_render:    The format of the return values.
+
+
+        :returns 'range':   :class:`DataRange <DataRange>`
+                 'cell':    [:class:`Cell <Cell>`]
+                 'matrix':  [[ ... ], [ ... ], ...]
         """
         if returnas == 'matrix':
             values = self.client.get_range(self.spreadsheet.id, self._get_range(start, end), majdim.upper(),
