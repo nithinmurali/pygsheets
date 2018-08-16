f663a7ffa4f1f213ce81b522cc33b3c7da71762a
Kordishal <jonaswaeber@gmail.com>
2018-08-16 08:55:49 +0200

    fix(auth): Fix authorization process
    Adds translation for deprecated kwargs and a warning.
    Adds the ability to pass in custom credentials.
    Fix docs.

--- pygsheets/authorization.py
+++ pygsheets/authorization.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-.
 import os
 import json
+import warnings
 
 from google.oauth2 import service_account
 from google.oauth2.credentials import Credentials
@@ -9,7 +11,7 @@ from pygsheets.client import Client
 
 
 def _get_user_authentication_credentials(client_secret_file, scopes, credential_directory=None):
-    """"""
+    """Returns user credentials."""
     if credential_directory is None:
         credential_directory = os.getcwd()
     elif credential_directory == 'global':
@@ -26,7 +28,8 @@ def _get_user_authentication_credentials(client_secret_file, scopes, credential_
         # expect these to be valid. may expire at some point, but should be refreshed by google api client...
         return Credentials.from_authorized_user_file(credentials_path, scopes=scopes)
 
-    flow = Flow.from_client_secrets_file(client_secret_file, scopes=scopes, redirect_uri='urn:ietf:wg:oauth:2.0:oob')
+    flow = Flow.from_client_secrets_file(client_secret_file, scopes=scopes,
+                                         redirect_uri='urn:ietf:wg:oauth:2.0:oob')
 
     auth_url, _ = flow.authorization_url(prompt='consent')
 
@@ -51,17 +54,48 @@ def _get_user_authentication_credentials(client_secret_file, scopes, credential_
     return credentials
 
 
-def authorize(client_secret='client_secret.json', service_account_file=None, credentials_directory='',
-              scopes=('https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive')):
-    """
+_SCOPES = ('https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive')
 
-    :param client_secret:           Path to client secret file for OAuth. Ignored if service_account_file is given.
-    :param service_account_file:    Path of the service account file.
-    :param credentials_directory:   Where credential-tokens are stored (only relevant for OAuth authentication).
-    :param scopes:
-    :return:
+_deprecated_keyword_mapping = {
+    'outh_file': 'client_secret',
+    'outh_creds_store': 'credentials_directory',
+    'outh_nonlocal': 'non_local_authorization',
+    'service_file': 'service_account_file',
+    'credentials': 'custom_credentials'
+}
+
+
+def authorize(client_secret='client_secret.json',
+              service_account_file=None,
+              credentials_directory='',
+              scopes=_SCOPES,
+              custom_credentials=None,
+              **kwargs):
+
+    """Authenticate this application with a google account.
+
+    See general authorization documentation for details on how to attain the necessary files.
+
+    :param client_secret:           Location of the oauth2 credentials file.
+    :param service_account_file:    Location of a service account file.
+    :param credentials_directory:   Location of the token file created by the OAuth2 process. Use 'global' to store in
+                                    global location, which is OS dependent. Default None will store token file in
+                                    current working directory.
+    :param custom_credentials:      A custom or pre-made credentials object. Will ignore all other params.
+    :param scopes:                  The scopes for which the authentication applies.
+    :param kwargs:                  Parameters to be handed into the client constructor.
+    :returns:                       :class:`Client`
     """
-    if service_account_file is not None:
+    v = vars()
+    for key in kwargs:
+        if key in ['outh_file', 'outh_creds_store', 'service_file', 'credentials']:
+            warnings.warn('The argument {} is deprecated. Use {} instead.'.format(key, _deprecated_keyword_mapping[key]))
+            v[_deprecated_keyword_mapping[key]] = kwargs[key]
+            del kwargs[key]
+            
+    if custom_credentials is not None:
+        credentials = custom_credentials
+    elif service_account_file is not None:
         credentials = service_account.Credentials.from_service_account_file(service_account_file, scopes=scopes)
     else:
         credentials = _get_user_authentication_credentials(client_secret, scopes, credentials_directory)
